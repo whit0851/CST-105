9dcc4d4e5796c1eb1593a4375fa77fd795693edf
BILL\Charles Whitten <whit0851@gmail.com>
2017-03-12 00:05:04 -0600

    Topic 3 Homework

---
+++ Topic 3/Chapter6_Question7.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Chapter6_Question7 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("The amount invested: ");
+		double invested = input.nextDouble();
+		System.out.print("Annual interest rate: ");
+		double interestrate = input.nextDouble();
+		input.close();
+		double mrate = (interestrate/(100*12));
+		System.out.print("Years     "+"Future Value\n");
+		
+		for (int i=1; i <= 30; i++){
+			invested = futureInvestmentValue(invested, mrate, 1);
+			System.out.print(i + "     " + invested + "\n" );
+			
+		}
+	}
+
+	public static double futureInvestmentValue(double investmentAmount, double monthlyInterestRate, int years){
+		
+		return investmentAmount * Math.pow(1 + monthlyInterestRate, years*12);
+	}
+	
+}
