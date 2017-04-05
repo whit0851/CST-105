9c34693c8811a9c6454c16a38ddeb28753fcd977
Charles Whitten <whit0851@gmail.com>
2017-04-05 17:45:09 -0500

    Topic 6 Homework

--- Topic 6/Octagon.java
+++ Topic 6/Octagon.java
@@ -8,6 +8,36 @@
  *
  * @author charlesw
  */
-public class Octagon {
+public class Octagon extends GeometricObject implements Cloneable, Comparable<Octagon>{
+    private double side;
+    private double area;
+    private double perimeter;
+    public Octagon(double side){
+    this.side = side; 
+    }
+    @Override
+    public double getArea() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double getPerimeter() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object clone()throws CloneNotSupportedException{
+        return super.clone();
+    }
+    @Override
+    public int compareTo(Octagon o) {
+        if (getArea() > o.getArea())
+            return 1;
+        else if (getArea() < o.getArea())
+            return -1;
+        else
+            return 0;
+        
+    }
     
 }
